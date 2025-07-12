dc24e92550dcfb9be3a18353933ca58f86d01d7c
Computhink-CS126 <cs126@computhink.com.sg>
2025-07-12 07:32:13 +0000

    Jul 12, 2025, 7:32 AM

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -6,9 +6,9 @@ function setup(){
 function draw(){
     fill(255, 255, 255);
     ellipse(300,200,200,200);
-    fill(255, 0, 0);
+    fill(127, 0, 0);
     ellipse(300,200,200,200);
-    fill(255, 0, 0);
+    fill(0, 0, 0);
     ellipse(300,200,200,200);
     // Todo: Challenge 3 Traffic Light
 }
